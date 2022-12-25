d50a6ff86e3b9f101bb0eafd561888cd58800409
ankitarangari <ankitarangari999@gmail.com>
2022-12-25 16:42:19 +0530

    my pp

---
+++ src/seleniumdemo/demo.java
@@ -0,0 +1,28 @@
+package seleniumdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo {
+
+	public static void main(String[]args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Downloads\\New folder\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
